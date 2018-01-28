25240a7d39e8a5bb3d4d7b9281c07bac825a5333
shnnxn <fmtalkk@gmail.com>
2018-01-28 04:56:03 -0800

    raw layout

--- layout.py
+++ layout.py
@@ -1,1 +1,13 @@
-print ("test commit")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return "haha"
+
+@app.route("/start")
+def start():
+  return "gg"
+
+app.run()

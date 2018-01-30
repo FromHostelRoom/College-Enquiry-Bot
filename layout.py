30fe2f711f6dbba42d0de85ba9f409de1a7d264c
shnnxn <fmtalkk@gmail.com>
2018-01-30 01:43:54 -0800

    basic layout ready

--- layout.py
+++ layout.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return "haha"
+  return render_template("welcome.html")
 
 @app.route("/start")
 def start():
